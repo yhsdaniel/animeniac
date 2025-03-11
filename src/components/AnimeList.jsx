f9f81e3d34cbd871c9b90e8fa6a2993b68664d89
terlanjurtampan36 <user36@realtechltd.com>
2025-03-11 10:27:21 +0700

    update

--- src/components/AnimeList.jsx
+++ src/components/AnimeList.jsx
@@ -8,7 +8,7 @@ export default function AnimeList({ index, onClick, src, title }) {
             as={motion.li}
             initial={{ opacity: 0, x: -100 }}
             whileInView={{ opacity: 1, x: 1 }}
-            transition={{ delay: index * 0.1, duration: 0.5, ease: 'easeInOut', type: "spring", stiffness: 100, damping: 15 }}
+            transition={{ delay: index * 0.1, duration: 0.5, ease: 'easeInOut', x: { type: "spring", bounce: 0 } }}
             viewport={{ once: true, amount: 0.2 }}
         >
             <Button onClick={onClick}>
